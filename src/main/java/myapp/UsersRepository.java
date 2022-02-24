f17e0f4c8f1e98f794f78484882f7395815d0e86
Jerry Klos <jerry.klos@ucdconnect.ie>
2022-02-24 11:55:56 +0000

    Changed User repository from Crud to Jpa

--- src/main/java/myapp/UsersRepository.java
+++ src/main/java/myapp/UsersRepository.java
@@ -1,9 +1,9 @@
 package myapp;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
-public interface UsersRepository extends CrudRepository<Users, Integer> {
+public interface UsersRepository extends JpaRepository<Users, Integer> {
     @Query("select u from Users u where u.username=?1")
     Users findByUsername(String username);
 }
