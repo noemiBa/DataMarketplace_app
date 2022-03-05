0b01e2c06dc7f0818690b7c9152a9f923b5c79a3
Noemi <banalnoemi@gmail.com>
2022-03-05 18:05:12 +0000

    can now change the quantity of the products in the shopping cart. Added shopping_cart.js file.

---
+++ src/main/resources/static/js/shopping_cart.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+	$(".minusButton").on("click", function(evt) {
+		evt.preventDefault(); 
+		decreaseQuantity($(this));
+	});
+
+	$(".plusButton").on("click", function(evt) {
+		evt.preventDefault(); 
+		increaseQuantity($(this));
+	});
+
+	updateTotal(); 
+});
+
+function decreaseQuantity(link) {
+	productId = link.attr("pid");
+	quantityInput = $("#quantity" + productId);
+		
+	newQuantity = parseInt(quantityInput.val())-1;
+	if (newQuantity > 0) {
+		quantityInput.val(newQuantity);
+	}	
+}
+
+function increaseQuantity(link) {
+	productId = link.attr("pid");
+	quantityInput = $("#quantity" + productId);
+		
+	newQuantity = parseInt(quantityInput.val())+1;
+	if (newQuantity < 100) {
+		quantityInput.val(newQuantity);
+	}	
+}
+
+
+function updateTotal() {
+	total = 0.0; 
+	$(".productSubtotal").each(function(index, element) {
+		total = total + parseFloat(element.innerHTML);
+	});
+	total = total.toFixed(2);
+	$("#totalAmount").text("€" + total);
+}
